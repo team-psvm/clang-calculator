f584c04b1a6d1c741c1d0aecc626771d6d18cbcc
zionhann <hanzion1108@gmail.com>
2024-03-26 13:29:17 +0900

    feat: 사칙 연산 메뉴 출력

--- fun.impl.c
+++ fun.impl.c
@@ -1,7 +1,12 @@
 #include "fun.h"
 
 void print_menu() {
-  // Your code here
+  printf("==Simple Calculator==\n");
+  printf("1. Addition\n");
+  printf("2. Subtraction\n");
+  printf("3. Multiplication\n");
+  printf("4. Division\n");
+  printf("5. Exit\n");
 }
 
 bool is_exit(int option) {
